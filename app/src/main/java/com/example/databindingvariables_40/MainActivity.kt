7bea42dfd1441e7f4621a40a6f0653244f5f4d0d
Camarederic <camarederic@mail.ru>
2022-11-05 19:41:37 +0300

    Create Data Binding Variables

--- app/src/main/java/com/example/databindingvariables_40/MainActivity.kt
+++ app/src/main/java/com/example/databindingvariables_40/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.databindingvariables_40
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import com.example.databindingvariables_40.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var myViewModel: MyViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        myViewModel = ViewModelProvider(this).get(MyViewModel::class.java)
+
+        binding.viewModel = myViewModel
+        binding.lifecycleOwner = this
+
+
+
+//        myViewModel.message.observe(this) {
+//            binding.textview.text = it
+//        }
+
+        binding.editText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                myViewModel.setMessage(s.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/example/databindingvariables_40/User.kt
@@ -0,0 +1,8 @@
+package com.example.databindingvariables_40
+
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val age: Int,
+    val loggedIn: Boolean
+)
